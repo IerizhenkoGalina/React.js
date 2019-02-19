f62a4aac0353fdd3ac3f70498332ce6862e4501e
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-19 08:42:23 +0200

    Create Variables in JSX.js

---
+++ Variables in JSX.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const theBestString = 'I live in the beautiful city!';
+
+ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
+
+//Prints: I live in the beautiful city!
