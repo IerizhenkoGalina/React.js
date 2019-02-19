de567fc8739dfd9fbd8f33fa3caafed8cf983d3c
IerizhenkoGalina <44175437+IerizhenkoGalina@users.noreply.github.com>
2019-02-19 09:33:44 +0200

    Create prop.js
    Once the browser refreshes, click on the button. Ew, how annoying!

---
+++ prop.js
@@ -0,0 +1,39 @@
+//We have two files:  Button.js and Talker.js
+/*
+import React from 'react';
+
+export class Button extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.onClick}>
+        Click me!
+      </button>
+    );
+  }
+}
+*/
+//second file:
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from './Button';
+
+class Talker extends React.Component {
+  handleClick() {
+    let speech = '';
+    for (let i = 0; i < 10; i++) {
+      speech += 'blah ';
+    }
+    alert(speech);
+  }
+  
+  render() {
+    return <Button onClick={this.handleClick}/>;
+  }
+}
+
+ReactDOM.render(
+  <Talker />,
+  document.getElementById('app')
+);
+*/
